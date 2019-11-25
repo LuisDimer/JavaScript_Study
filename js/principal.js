f4d79b793cfdf24f32bda0f3fc6f16be3b84369e
Luis Fernando Dimer da Silva <luis.silva@herval.com.br>
2019-11-25 18:54:10 -0200

    commit

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,4 @@
-var paciente = document.querySelectorAll(".paciente");
+//var paciente = document.querySelectorAll(".paciente");
 
 //debugger;
 
@@ -37,15 +37,29 @@ var paciente = document.querySelectorAll(".paciente");
 //     }
 // }
 
+var paciente = document.querySelectorAll(".paciente");
+// criando variavel = seleciona o documento no caso o html.função seleciona tudo que tem a classe("classe")
+// RESULTADO: variável paciente = todo o conteudo que tem a classe ".paciente".
+
 for(var i = 0; i <= paciente.length; i ++) {
+//função de loop (criando variavel "i" = igual a zero; "i" menor ou igual ao tamanho da array paciente; variavel acrescenta um)
+//RESULTADO: o que estiver dentro desse loop vai repetir até a variável "i" ser maior do que a quantidade de array que tem a variavel paciente.
 
-var tdPeso = paciente[i].querySelector(".info-peso");
+    var tdPeso = paciente[i].querySelector(".info-peso");
+    //Criação variavel "tdPeso" = classe html "paciente" numero que esta nessa condição [i] que no caso é o numero que esta o indice do loop.querySelector seleciona o conteudo de paciente[i] ".info-peso"  
+    //RESULTADO: variavel tdPeso recebe o conteúdo da classe html ".info-peso" que está dentro da classe paciente da array do indice [i]
 
- var peso = tdPeso.textContent;
+    var peso = tdPeso.textContent;
+    //Criação variável "Peso" = variável "tdPeso"."textContent" 
+    //RESULTADO: variavel "peso" recebe conteudo de "tdPeso" que possui o conteúdo de ".info-peso"
 
     var tdAltura = paciente[i].querySelector(".info-altura");
+    //Criação variavel "tdAltura" = classe html "paciente" numero que esta nessa condição [i] que no caso é o numero que esta o indice do loop.querySelector seleciona o conteudo de paciente[i] ".info-peso"  
+    //RESULTADO: variavel tdAltura recebe o conteúdo da classe html ".info-altura" que está dentro da classe paciente da array do indice [i]
 
     var altura = tdAltura.textContent;
+    //Criação variável "altura" = variável "tdAltura"."textContent" 
+    //RESULTADO: variavel "peso" recebe conteudo de "tdPeso" que possui o conteúdo de ".info-peso"
 
     var imc = peso / (altura * altura);
 
@@ -71,3 +85,9 @@ var tdPeso = paciente[i].querySelector(".info-peso");
         tdIMC.textContent = imc.toFixed(2);   
     }
 }
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("Click", function(event){
+    console.log("Botão foi clicado");
+});
